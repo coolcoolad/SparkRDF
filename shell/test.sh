085f3dc935f01f42079d0539da72bf2fe6714e50
coolcoolad <441629632@qq.com>
2016-04-05 19:42:27 +0800

    'ss'

---
+++ shell/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export SPARK_HOME=/home/xiaoju/spark-1.6.0
+export PATH=$SPARK_HOME/bin:$PATH
+
+spark-submit -v \
+    --class yangjie.rdf.main.OwlToTriple \
+    --driver-memory 4g \
+    --queue strategy \
+    --conf "spark.dynamicAllocation.minExecutors=10" \
+    --conf "spark.dynamicAllocation.maxExecutors=40" \
+    SparkRdf-1.0-SNAPSHOT.jar
