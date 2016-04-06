5ad0f9b2755db7f7d49110d1018aa0454c2d96bb
coolcoolad <441629632@qq.com>
2016-04-06 13:12:01 +0800

    'ss'

--- src/main/scala/yangjie/rdf/main/OwlToTriple.scala
+++ src/main/scala/yangjie/rdf/main/OwlToTriple.scala
@@ -2,7 +2,7 @@ package yangjie.rdf.main
 
 import org.apache.spark.{SparkConf, SparkContext}
 import yangjie.rdf.utils.IoHelper
-import scala.xml.XML
+import scala.xml.{XML,Node}
 
 /**
   * Created by yangjiecloud on 2016/4/5.
@@ -13,22 +13,33 @@ object OwlToTriple {
     val sc = new SparkContext(conf)
 
     val fPath = "/user/yangjiecloud/SparkRdf/owl"
+    val rdfPrefix = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    def textFunc(chd:Node) = chd.label
+    def resourceFunc(chd:Node) = chd.attribute(rdfPrefix, "resource")
+    def childFunc(chd:Node) = chd.child.head.attribute(rdfPrefix, "resource")
+
     var rdd = sc.wholeTextFiles(fPath)
-    rdd = rdd.flatMapValues(doc => {
+    rdd = rdd.flatMap{case (key,doc) => {
       val xml = XML.loadString(doc)
-      val entityNode = (xml \\ "_").filter(x => x.prefix == "ub" && x.attribute("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "about") != None)
-      val entityKv = entityNode.map(x => (x.attribute("http://www.w3.org/1999/02/22-rdf-syntax-ns#", "about"), x.label))
-      entityKv.map(kv => {
-        val key = kv._1 match {
-          case Some(x) => x
-          case None => "null"
-        }
-        s"${key},${kv._2}"
+      val entityNode = (xml \ "_").filter(x => x.prefix == "ub" && x.attribute(rdfPrefix, "about") != None)
+      entityNode.flatMap(node => {
+        node.child.map(chd => {
+          val uri = chd.child.isEmpty match {
+            case false => childFunc(chd)
+            case _ => {
+              chd.attribute(rdfPrefix, "resource") match {
+                case null => textFunc(chd)
+                case _ => resourceFunc(chd)
+              }
+            }
+          }
+          (node.label,s"${chd.label},${uri}")
+        })
       })
-    })
+    }}
     val outPath = "/user/yangjiecloud/SparkRdf/triple"
-    println(IoHelper.deleteFileInHDFS(fPath))
-    rdd.saveAsTextFile(fPath)
+    println(IoHelper.deleteFileInHDFS(outPath))
+    rdd.saveAsTextFile(outPath)
     sc.stop()
     println("over")
   }
